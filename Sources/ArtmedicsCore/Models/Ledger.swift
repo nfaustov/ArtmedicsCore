18fe77f6ba7a2a16e5f4356d00a43d5a337c871a
Nikolai Faustov <faustov.nikolay@gmail.com>
2023-04-14 23:59:55 +0300

    Modified Ledger

--- Sources/ArtmedicsCore/Models/Ledger.swift
+++ Sources/ArtmedicsCore/Models/Ledger.swift
@@ -1,18 +1,39 @@
 import Foundation
 
-public struct Ledger: Codable, Hashable {
+public struct Ledger: Codable, Hashable, Identifiable {
+    public let id: UUID
     public let date: Date
-    public var balance: Double
+    public var cashBalance: Double
+    public var bankBalance: Double
+    public var cardBalance: Double
     public var payments: [Payment] = []
+
+    public var balance: Double {
+        cashBalance + bankBalance + cardBalance
+    }
 
     public mutating func payment(_ payment: Payment) {
         payments.append(payment)
-        balance += payment.amount
+
+        switch payment.type {
+        case .cash: cashBalance += payment.amount
+        case .bank: bankBalance += payment.amount
+        case .card: cardBalance += payment.amount
+        }
     }
 }
 
 public struct Payment: Codable, Hashable, Identifiable {
     public let id: UUID
     public let title: String
+    public let type: PaymentType
     public let amount: Double
+}
+
+public extension Payment {
+    enum PaymentType: Codable, Hashable {
+        case cash
+        case bank
+        case card
+    }
 }
