be71d7d5172231803ac015abe303512cffc65194
Nikolai Faustov <faustov.nikolay@gmail.com>
2023-05-04 13:06:09 +0300

    Added status to patients appointment

--- Sources/ArtmedicsCore/Models/PatientAppointment.swift
+++ Sources/ArtmedicsCore/Models/PatientAppointment.swift
@@ -1,15 +1,26 @@
 import Foundation
 
 public struct PatientAppointment: Codable, Hashable, Identifiable {
+    public enum Status: String, Codable {
+        case none
+        case registered = "Зарегистрирован"
+        case congirmed = "Подтвержден"
+        case came = "Пришел"
+        case completed = "Завершен"
+        case cancelled = "Отменен"
+    }
+
     public let id: UUID
     public var scheduledTime: Date
     public var duration: TimeInterval
     public var patient: Patient?
+    public var status: Status
 
-    public init(id: UUID = UUID(), scheduledTime: Date, duration: TimeInterval, patient: Patient?) {
+    public init(id: UUID = UUID(), scheduledTime: Date, duration: TimeInterval, patient: Patient?, status: Status = .none) {
         self.id = id
         self.scheduledTime = scheduledTime
         self.duration = duration
         self.patient = patient
+        self.status = status
     }
 }
