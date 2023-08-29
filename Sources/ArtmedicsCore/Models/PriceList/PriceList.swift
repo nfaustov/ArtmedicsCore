f2aa4317bbd12adb1f7a1d6bee93e2394e6eb59f
Nikolai Faustov <faustov.nikolay@gmail.com>
2023-08-29 15:18:26 +0300

    Created dbModel for Category, updates

---
+++ Sources/ArtmedicsCore/Models/PriceList/PriceList.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+public final class PriceList {
+    public var categories: [Category]
+
+    public var items: [PriceListItem] {
+        categories.flatMap { $0.items }
+    }
+
+    public init(categories: [Category]) {
+        self.categories = categories
+    }
+
+    public func filteredCategories(byItem filterText: String) -> [Category] {
+        if filterText.isEmpty {
+            return categories
+        } else {
+            return categories.filter { category in
+                !category.items
+                    .filter {
+                        $0.title.localizedCaseInsensitiveContains(filterText) || $0.id.localizedCaseInsensitiveContains(filterText)
+                    }
+                    .isEmpty
+            }
+        }
+    }
+}
