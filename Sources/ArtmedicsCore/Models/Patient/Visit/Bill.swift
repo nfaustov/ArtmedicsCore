45f8c71bd37b19bb13bbe069680bd2437c2971ad
Nikolai Faustov <faustov.nikolay@gmail.com>
2023-09-04 10:32:54 +0300

    Modified RenderedServcie, replaces

--- Sources/ArtmedicsCore/Models/Patient/Visit/Bill.swift
+++ Sources/ArtmedicsCore/Models/Patient/Visit/Bill.swift
@@ -1,19 +1,5 @@
 import Foundation
 
-public struct Visit: Codable, Hashable, Identifiable {
-    public let id: UUID
-    public let registrationDate: Date
-    public let visitDate: Date
-    public var bill: Bill?
-
-    public init(id: UUID = UUID(), registrationDate: Date, visitDate: Date, bill: Bill? = nil) {
-        self.id = id
-        self.registrationDate = registrationDate
-        self.visitDate = visitDate
-        self.bill = bill
-    }
-}
-
 public struct Bill: Codable, Hashable, Identifiable {
     public let id: UUID
     public var services: [RenderedService]
--- Sources/ArtmedicsCore/Models/Patient/Visit/RenderedService.swift
+++ Sources/ArtmedicsCore/Models/Patient/Visit/RenderedService.swift
@@ -2,7 +2,6 @@ import Foundation
 
 public struct RenderedService: Codable, Hashable, Identifiable {
     public let id: UUID
-    public let categoryTitle: String
     public let pricelistItem: PriceList.Item
     public var performer: AnyEmployee
     public var agent: AnyEmployee?
@@ -10,14 +9,12 @@ public struct RenderedService: Codable, Hashable, Identifiable {
 
     public init(
         id: UUID = UUID(),
-        categoryTitle: String,
         pricelistItem: PriceList.Item,
         performer: AnyEmployee,
         agent: AnyEmployee? = nil,
         conclusion: Data? = nil
     ) {
         self.id = id
-        self.categoryTitle = categoryTitle
         self.pricelistItem = pricelistItem
         self.performer = performer
         self.agent = agent
