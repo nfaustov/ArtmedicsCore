8e77ad2789818e95556213c141db646a8e9d4a32
Nikolai Faustov <faustov.nikolay@gmail.com>
2023-09-14 14:40:11 +0300

    Added discountRate property

--- Sources/ArtmedicsCore/Models/Patient/Visit/Bill.swift
+++ Sources/ArtmedicsCore/Models/Patient/Visit/Bill.swift
@@ -17,6 +17,11 @@ public struct Bill: Codable, Hashable, Identifiable {
         price - discount - balance
     }
 
+    public var discountRate: Double {
+        guard price != 0 else { return 0 }
+        return discount / price
+    }
+
     public init(id: UUID = UUID(), services: [RenderedService], discount: Double = 0, balance: Double = 0, contract: Data? = nil) {
         self.id = id
         self.services = services
