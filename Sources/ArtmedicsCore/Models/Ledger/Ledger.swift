06de5cfddd1804df8527c6668c8a06a0f141c7bd
Nikolai Faustov <faustov.nikolay@gmail.com>
2023-09-15 10:59:39 +0300

    Modified PriceList.Item, updated Ledger

--- Sources/ArtmedicsCore/Models/Ledger/Ledger.swift
+++ Sources/ArtmedicsCore/Models/Ledger/Ledger.swift
@@ -17,6 +17,61 @@ public final class Ledger: Codable {
         guard reporting(.income) > 0 else { return 0 }
         return reporting(.income, of: type) / reporting(.income)
     }
+
+    public func pricelistItemUsage(id: String, period: DateInterval) -> Int {
+        servicesWithPricelistItem(id: id, period: period).count
+    }
+
+    public func pricelistItemIncome(id: String, period: DateInterval) -> Double {
+        servicesWithPricelistItem(id: id, period: period)
+            .map { $0.pricelistItem.price }
+            .reduce(0.0, +)
+    }
+
+    public func pricelistItemProfit(id: String, period: DateInterval) -> Double {
+        var servicesCostPrice = 0.0
+        var salaryForServices = 0.0
+
+        for service in servicesWithPricelistItem(id: id, period: period) {
+            switch service.performer.salary {
+            case let .pieceRate(rate):
+                salaryForServices += service.pricelistItem.price * rate
+            default:()
+            }
+
+            if let agent = service.agent {
+                salaryForServices += service.pricelistItem.price * 0.1
+            }
+
+            servicesCostPrice += service.pricelistItem.costPrice
+        }
+
+        let spendings = salaryForServices + servicesCostPrice + discountForPricelistItem(id: id, period: period)
+
+        return pricelistItemIncome(id: id, period: period) - spendings
+    }
+}
+
+private extension Ledger {
+    func billsCollection(period: DateInterval) -> [Bill] {
+        reports
+            .filter { $0.date > period.start && $0.date < period.end}
+            .flatMap { $0.payments }
+            .compactMap { $0.bill }
+    }
+
+    func discountForPricelistItem(id: String, period: DateInterval) -> Double {
+        billsCollection(period: period)
+            .filter { $0.services.contains(where: { $0.pricelistItem.id == id }) }
+            .map { $0.discount }
+            .reduce(0.0, +)
+    }
+
+    func servicesWithPricelistItem(id: String, period: DateInterval) -> [RenderedService] {
+        billsCollection(period: period)
+            .flatMap { $0.services }
+            .filter { $0.pricelistItem.id == id }
+    }
 }
 
 public enum Reporting: String, Hashable, Identifiable, CaseIterable {
--- Sources/ArtmedicsCore/Models/PriceList/PriceList.Item.swift
+++ Sources/ArtmedicsCore/Models/PriceList/PriceList.Item.swift
@@ -6,14 +6,12 @@ public extension PriceList {
         public var title: String
         public var price: Double
         public var costPrice: Double
-        public var used: Int
 
-        public init(id: String, title: String, price: Double, costPrice: Double = 0, used: Int = 0) {
+        public init(id: String, title: String, price: Double, costPrice: Double = 0) {
             self.id = id
             self.title = title
             self.price = price
             self.costPrice = costPrice
-            self.used = used
         }
     }
 }
