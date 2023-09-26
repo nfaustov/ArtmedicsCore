7154ebfc6a354f4127b00446b7212af148ad3658
Nikolai Faustov <faustov.nikolay@gmail.com>
2023-09-26 12:56:04 +0300

    Added title property to servicesSet

--- Sources/ArtmedicsCore/Models/PriceList/PriceList.swift
+++ Sources/ArtmedicsCore/Models/PriceList/PriceList.swift
@@ -38,10 +38,12 @@ public final class PriceList {
 
 public struct ServicesSet: Codable, Hashable, Identifiable {
     public let id: UUID
+    public let title: String
     public var services: [RenderedService]
 
-    public init(id: UUID = UUID(), services: [RenderedService] = []) {
+    public init(id: UUID = UUID(), title: String, services: [RenderedService] = []) {
         self.id = id
+        self.title = title
         self.services = services
     }
 }
